754e09939211abe2ee79b08b611d273f846998d8
Brad House <brad@brad-house.com>
2025-01-21 13:41:08 -0500

    updates for mellanox

---
+++ roles/sonic/files/port_perm_macaddr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+has_cnt=0
+
+echo "{"
+while read intf ; do
+  permaddr=`ethtool -P ${intf} | cut -d: -f2- | sed -e 's/ //g'`
+  if [ ${has_cnt} = 1 ]; then
+    echo ","
+  fi
+  cat << EOF
+  "${intf}" : "${permaddr}"
+EOF
+  has_cnt=1
+done < <(ip a | grep Ethernet | sed -E 's/[0-9]+: (Ethernet[0-9]+):.*/\1/')
+
+echo "}"
