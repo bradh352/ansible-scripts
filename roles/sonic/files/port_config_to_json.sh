1015d907e486b138638ee890d7a90e7dbeb62c98
Brad House <brad@brad-house.com>
2025-04-18 18:29:24 -0400

    sonic fixes

--- roles/sonic/files/port_config_to_json.sh
+++ roles/sonic/files/port_config_to_json.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-grep -v -e '^#.*' -e '^ *$' "$1" | awk 'BEGIN { ORS = ""; print " [ "} NR > 1 {printf "," } { printf " { \"port\" : \""$1"\", \"lanes\" : \""$2"\", \"alias\" : \""$3"\", \"index\" : \""$4"\", \"speed\" : \""$5"\"}" } END { print " ] " }'
+fields=`grep -v -e '^#.*' -e '^ *$' "$1" | head -n 1 | awk '{ print NF }'`
+if [ $fields -lt 4 ] ; then
+	grep -v -e '^#.*' -e '^ *$' "$1" | awk 'BEGIN { ORS = ""; print " [ "} NR > 1 {printf "," } { printf " { \"port\" : \""$1"\", \"lanes\" : \""$2"\", \"alias\" : \"Eth"$3"\", \"index\" : \""$3"\", \"speed\" : \"10000\"}" } END { print " ] " }'
+else
+	grep -v -e '^#.*' -e '^ *$' "$1" | awk 'BEGIN { ORS = ""; print " [ "} NR > 1 {printf "," } { printf " { \"port\" : \""$1"\", \"lanes\" : \""$2"\", \"alias\" : \""$3"\", \"index\" : \""$4"\", \"speed\" : \""$5"\"}" } END { print " ] " }'
+fi
