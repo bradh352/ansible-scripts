ff10255da0ea937dc98abe99a4fdb9f738834c0e
Brad House <brad@brad-house.com>
2025-05-26 12:36:59 -0400

    move base role to submodule

--- roles/base/files/99-prompt.sh
+++
@@ -1,3 +0,0 @@
-myname=`hostname -f | cut -d. -f1-2`
-export PS1="[\u@${myname} \W]\\$ "
-unset myname
--- roles/base/files/trim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat /proc/mounts | grep ^/ | grep -e " xfs " -e " ext4 " | awk '{ print $2 };' | while read mountpoint ; do
-  ionice -c2 -n7 nice -n 19 fstrim ${mountpoint}
-done 
